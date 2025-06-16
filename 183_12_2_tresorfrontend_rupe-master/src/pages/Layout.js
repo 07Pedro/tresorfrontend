c0c412ef0ef8b2d185382882571309a0294cb4b2
Petr Cerny <petr.cerny@e3ag.ch>
2025-06-16 15:04:42 +0200

    changed UI of navbar

--- 183_12_2_tresorfrontend_rupe-master/src/pages/Layout.js
+++ 183_12_2_tresorfrontend_rupe-master/src/pages/Layout.js
@@ -1,47 +1,57 @@
 import { Outlet, Link } from "react-router-dom";
+import { useState } from "react";
+import "../css/mvp.css";
 
-/**
- * Layout
- * @author Peter Rutschmann
- */
-const Layout = ({loginValues}) => {
+const Layout = ({ loginValues }) => {
+    const [activeDropdown, setActiveDropdown] = useState(null);
+
+    const toggleDropdown = (menu) => {
+        setActiveDropdown((prev) => (prev === menu ? null : menu));
+    };
+
     return (
-        <>
-            <nav>
-                <h1>The secret tresor application</h1>
-                <p>{loginValues.email === '' ? 'No user logged in' : 'user:' + loginValues.email}</p>
-                <ul>
-                    <li><a href="/">Secrets</a>
-                    <ul>
-                        <li><Link to="/secret/secrets">my secrets</Link></li>
-                        <li><Link to="/secret/newcredential">new credential</Link></li>
-                        <li><Link to="/secret/newcreditcard">new credit-card</Link></li>
-                        <li><Link to="/secret/newnote">new note</Link></li>
-                    </ul>
-                    </li>
-                    <li><a href="/">User</a>
-                    <ul>
-                        <li><Link to="/user/login">login</Link></li>
-                        <li><Link to="/user/register">register</Link></li>
-                    </ul>
-                    </li>
-                    <li><a href="/">Admin</a>
-                        <ul>
-                            <li><Link to="/user/users">All users</Link></li>
-                            <li>Add user</li>
-                            <li><Link to="/user/users/:id">Edit user</Link></li>
-                            <li>All secrets</li>
+        <div style={{ fontFamily: "sans-serif", minHeight: "100vh", backgroundColor: "#f5f5f5" }}>
+            <nav className="navbar">
+                <div className="nav-header">
+                    <h1>🔐 Secret Tresor</h1>
+                    <p>{loginValues.email === '' ? 'No user logged in' : `User: ${loginValues.email}`}</p>
+                </div>
+                <ul className="nav-list">
+                    <li className="nav-item dropdown">
+                        <span>Secrets ▾</span>
+                        <ul className="dropdown-menu">
+                            <li><Link to="/secret/secrets">My Secrets</Link></li>
+                            <li><Link to="/secret/newcredential">New Credential</Link></li>
+                            <li><Link to="/secret/newcreditcard">New Credit Card</Link></li>
+                            <li><Link to="/secret/newnote">New Note</Link></li>
                         </ul>
                     </li>
-                    <li>
+                    <li className="nav-item dropdown">
+                        <span>User ▾</span>
+                        <ul className="dropdown-menu">
+                            <li><Link to="/user/login">Login</Link></li>
+                            <li><Link to="/user/register">Register</Link></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <span>Admin ▾</span>
+                        <ul className="dropdown-menu">
+                            <li><Link to="/user/users">All Users</Link></li>
+                            <li>Add User</li>
+                            <li><Link to="/user/users/:id">Edit User</Link></li>
+                            <li>All Secrets</li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
                         <Link to="/">About</Link>
                     </li>
                 </ul>
             </nav>
-            <hr/>
-            <Outlet/>
-        </>
-    )
+            <main style={{ padding: "2rem" }}>
+                <Outlet />
+            </main>
+        </div>
+    );
 };
 
 export default Layout;
